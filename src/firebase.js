ebef4d502b9247d721d7f2056674955983f3586d
KevinC555 <kaseycorcoran95@bellsouth.net>
2022-03-31 10:18:19 -0500

    deploy my amazon clone to github pages

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBORW9jxMYF1uVFWSnWerM7Wkr9QjfPrsg",
+	authDomain: "challenge-acbc7.firebaseapp.com",
+	projectId: "challenge-acbc7",
+	storageBucket: "challenge-acbc7.appspot.com",
+	messagingSenderId: "400892816843",
+	appId: "1:400892816843:web:87babd57f7e5e7e3b5da4e",
+	measurementId: "G-LLWG2JGVGC"
+};
+
+export default firebaseConfig;
